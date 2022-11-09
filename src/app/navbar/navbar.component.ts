764ee156a82ad0182636d6530364d6f86a7d2125
Racz Csaba <legoscsabi@gmail.com>
2022-11-09 20:09:03 +0100

    beautiful navbar
    fájt és lett indokolatlan felesleges bootstrappem

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -16,7 +16,6 @@ interface navbar {
 export class NavbarComponent implements OnInit {
 
   LINKS: navbar[] = [
-    { url: '', title: 'Főpldal' },
     { url: '/energetikai', title: 'Energetikai Tanusítás' },
     { url: '/klimatechnika', title: 'Klímatechnika' },
     { url: '/hutestechnika', title: 'Hűtéstechnika' },
@@ -28,11 +27,12 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  //navcuccok
   navbarfixed:boolean = false;
   @ViewChild('toppic')toppic: any; //Elementreffet nemértem :(
   height:number=10;
   @HostListener('window:scroll',['$event']) onscroll(){
-    this.height = this.toppic.nativeElement.offsetHeight * 0.8
+    this.height = this.toppic.nativeElement.offsetHeight * 0.5
     if(window.scrollY > this.height)
     {
       this.navbarfixed = true;
@@ -40,6 +40,19 @@ export class NavbarComponent implements OnInit {
     else
     {
       this.navbarfixed = false;
+    }
+  }
+
+  //responsive cuccok
+  navbarOpen = false;
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+
+  alttogle() {
+    if (this.navbarOpen) {
+      this.navbarOpen = false;
     }
   }
 }
